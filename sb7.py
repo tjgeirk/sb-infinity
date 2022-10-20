37ce707aa903a5e61c5addd49b5fe5e6396aab5e
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-10-20 02:47:11 -0700

    Update sb7.py

--- sb7.py
+++ sb7.py
@@ -1,19 +1,16 @@
 import datetime
 import time
-
 from ccxt import kucoinfutures as kcf
 from pandas import DataFrame as dataframe
 from ta import trend, volatility, momentum
-from pprint import pp
-
 API_KEY = ''
 API_SECRET = ''
 API_PASSWD = ''
 COIN = 'BTC'
-LOTSPERTRADE = 1
-LEVERAGE = 20
+LOTSPERTRADE = 10
+LEVERAGE = 100
 TF = '5m'
-STOPLOSS = 0.03
+STOPLOSS = 0.05
 exchange = kcf({
     'adjusTForTimeDifference': True,
     'apiKey': API_KEY,
@@ -48,7 +45,7 @@ def buy():
     if side != 'short':
         amount = LOTSPERTRADE
         params = {'leverage': LEVERAGE}
-    return exchange.create_limit_buy_order(COIN, amount, price, params=params)
+    return exchange.create_limit_buy_order(coin, amount, price, params=params)
 
 
 def sell():
@@ -84,45 +81,42 @@ def lowerband(h, l, c, window):
 cycle = trail = 0
 stop = trail - abs(STOPLOSS)
 print('trader started')
-
 while True:
+    positions = exchange.fetch_positions()
+    cycle += 1
+    if cycle % 50 == 0:
+        print('clearing queue')
+        exchange.cancel_all_orders()
+    coin = str(f'{COIN}/USDT:USDT')
+    pnl = 0
+    contracts = 0
+    side = 'none'
+    trail = 0
+    for i, v in enumerate(positions):
+        if v['symbol'] == str(f'{COIN}/USDT:USDT'):
+            coin = v['symbol']
+            pnl = v['percentage']
+            contracts = v['contracts']
+            side = v['side']
+    trail = pnl if pnl > trail else trail
+    stop = trail - abs(STOPLOSS)
+    if pnl < stop and side == 'long':
+        sell()
+    if pnl < stop and side == 'short':
+        buy()
+    o = getData(coin, TF)['open']
+    h = getData(coin, TF)['high']
+    l = getData(coin, TF)['low']
+    c = getData(coin, TF)['close']
+    High = h.iloc[-1]
+    Low = l.iloc[-1]
+    Open = (c.iloc[-2]+o.iloc[-2])/2
+    Close = (c.iloc[-1]+h.iloc[-1]+l.iloc[-1])/3
     try:
-        positions = exchange.fetch_positions()
-        cycle += 1
-        if cycle % 50 == 0:
-            print('clearing queue')
-            exchange.cancel_all_orders()
-        coin = str(f'{COIN}/USDT:USDT')
-        pnl = 0
-        contracts = 0
-        side = 'none'
-        trail = pnl if pnl > trail else trail
-        stop = trail - abs(STOPLOSS)
-        for i, v in enumerate(positions):
-            if v['symbol'] == str(f'{COIN}/USDT:USDT'):
-                pnl = v['percentage']
-                contracts = v['contracts']
-                side = v['side']
-                trail = pnl if pnl > trail else trail
-                stop = trail - abs(STOPLOSS)
-            if pnl < stop and side == 'long':
-                sell()
-            if pnl < stop and side == 'short':
-                buy()
-
-            o = getData(coin, TF)['open']
-            h = getData(coin, TF)['high']
-            l = getData(coin, TF)['low']
-            c = getData(coin, TF)['close']
-            High = h.iloc[-1]
-            Low = l.iloc[-1]
-            Open = (c.iloc[-2]+o.iloc[-2])/2
-            Close = (c.iloc[-1]+h.iloc[-1]+l.iloc[-1])/3
-
-            if Open > upperband(h, l, c, 20) and Open > Close and rsi(c, 5) < 70:
-                sell()
-            if Open < lowerband(h, l, c, 20) and Open < Close and rsi(c, 5) > 30:
-                buy()
+        if Open > upperband(h, l, c, 20) and Open > Close and rsi(c, 5) < 70:
+            sell()
+        if Open < lowerband(h, l, c, 20) and Open < Close and rsi(c, 5) > 30:
+            buy()
     except Exception as e:
-        time.sleep(10)
         print(e)
+        time.sleep(10)
