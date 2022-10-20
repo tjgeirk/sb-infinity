ffb08646547e33a72a5731bfedc3769958026419
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-10-20 02:08:49 -0700

    Update sb7.py

--- sb7.py
+++ sb7.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 
-from ccxt import kucoinfutures as kucoin
+from ccxt import kucoinfutures as kcf
 from pandas import DataFrame as dataframe
 from ta import trend, volatility, momentum
 from pprint import pp
@@ -9,13 +9,12 @@ from pprint import pp
 API_KEY = ''
 API_SECRET = ''
 API_PASSWD = ''
-COINS = ['ETH', 'BTC']
-LOTSPERTRADE = 10
-LEVERAGE = 50
+COIN = 'BTC'
+LOTSPERTRADE = 1
+LEVERAGE = 20
 TF = '5m'
-STOPLOSS = 0.1
-TAKEPROFIT = 0.1
-exchange = kucoin({
+STOPLOSS = 0.03
+exchange = kcf({
     'adjusTForTimeDifference': True,
     'apiKey': API_KEY,
     'secret': API_SECRET,
@@ -43,17 +42,21 @@ def buy(coin, contracts, side):
     price = exchange.fetch_order_book(coin)['bids'][0][0]
     if side == 'short':
         amount = contracts
+        trail = 0
+        stop = trail - abs(STOPLOSS)
         params = {'reduceOnly': True, 'closeOrder': True}
     if side != 'short':
         amount = LOTSPERTRADE
         params = {'leverage': LEVERAGE}
-    return exchange.create_limit_buy_order(coin, amount, price, params=params)
+    return exchange.create_limit_buy_order(COIN, amount, price, params=params)
 
 
 def sell(coin, contracts, side):
     print(f'selling {coin}')
     price = exchange.fetch_order_book(coin)['asks'][0][0]
     if side == 'long':
+        trail = 0
+        stop = trail - abs(STOPLOSS)
         amount = contracts
         params = {'reduceOnly': True, 'closeOrder': True}
     if side != 'long':
@@ -78,56 +81,48 @@ def lowerband(h, l, c, window):
     return volatility.keltner_channel_lband(h, l, c, window).iloc[-1]
 
 
-def bot(coin, contracts, side):
-    o = getData(coin, TF)['open']
-    h = getData(coin, TF)['high']
-    l = getData(coin, TF)['low']
-    c = getData(coin, TF)['close']
-    High = h.iloc[-1]
-    Low = l.iloc[-1]
-    Open = (c.iloc[-2]+o.iloc[-2])/2
-    Close = (c.iloc[-1]+h.iloc[-1]+l.iloc[-1])/3
+cycle = trail = 0
+stop = trail - abs(STOPLOSS)
+print('trader started')
 
-    try:
-        if crypto['pnl'] < -abs(crypto['stop']) or crypto['pnl'] > TAKEPROFIT:
-            sell(coin, contracts, side) if side == 'long' else buy(
-                coin, contracts, side)
-
-        if Open > upperband(h, l, c, 20) and Open > Close and rsi(c, 5) < 70:
-            sell(coin, contracts, side)
-
-        if Open < lowerband(h, l, c, 20) and Open < Close and rsi(c, 5) > 30:
-            buy(coin, contracts, side)
-
-    except Exception as e:
-        print(e)
-
-
-cycle = 0
-coin = {}
-for symbol in COINS:
-    coin[symbol] = {'symbol': str(f'{symbol}/USDT:USDT'), 'side': 'none',
-                    'contracts': 0, 'pnl': 0, 'trail': 0, 'stop': -abs(STOPLOSS)}
 while True:
-    positions = exchange.fetch_positions()
-    for symbol in COINS:
-        crypto = coin[symbol]
+    try:
+        positions = exchange.fetch_positions()
         cycle += 1
         if cycle % 50 == 0:
-            print('still running :) -- clearing queue')
-            exchange.cancel_all_orders(crypto['symbol'])
+            print('clearing queue')
+            exchange.cancel_all_orders()
+        coin = str(f'{COIN}/USDT:USDT')
+        pnl = 0
+        contracts = 0
+        side = 'none'
+        trail = pnl if pnl > trail else trail
+        stop = trail - abs(STOPLOSS)
         for i, v in enumerate(positions):
-            if v['symbol'] == crypto['symbol']:
-                crypto.update({'pnl': v['percentage']})
-                crypto.update({'contracts': v['contracts']})
-                crypto.update({'side': v['side']})
-            if v['percentage'] > crypto['trail']:
-                crypto.update({'trail': v['percentage']})
-                crypto.update({'stop': (crypto['trail']-abs(STOPLOSS))})
-            if crypto['pnl'] <= crypto['stop']:
-                if crypto['side'] == 'long':
-                    sell(getData(crypto['symbol'], TF)['close'].iloc[-1])
-                if crypto['side'] == 'short':
-                    buy(getData(crypto['symbol'], TF)['close'].iloc[-1])
-        
-        bot(crypto['symbol'], crypto['contracts'], crypto['side'])
+            if v['symbol'] == str(f'{COIN}/USDT:USDT'):
+                pnl = v['percentage']
+                contracts = v['contracts']
+                side = v['side']
+                trail = pnl if pnl > trail else trail
+                stop = trail - abs(STOPLOSS)
+            if pnl < stop and side == 'long':
+                sell(coin, contracts, side)
+            if pnl < stop and side == 'short':
+                buy(coin, contracts, side)
+
+            o = getData(coin, TF)['open']
+            h = getData(coin, TF)['high']
+            l = getData(coin, TF)['low']
+            c = getData(coin, TF)['close']
+            High = h.iloc[-1]
+            Low = l.iloc[-1]
+            Open = (c.iloc[-2]+o.iloc[-2])/2
+            Close = (c.iloc[-1]+h.iloc[-1]+l.iloc[-1])/3
+
+            if Open > upperband(h, l, c, 20) and Open > Close and rsi(c, 5) < 70:
+                sell(coin, contracts, side)
+            if Open < lowerband(h, l, c, 20) and Open < Close and rsi(c, 5) > 30:
+                buy(coin, contracts, side)
+    except Exception as e:
+        time.sleep(10)
+        print(e)
