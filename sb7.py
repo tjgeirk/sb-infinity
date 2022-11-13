feedc8d351317e85f6821625eeb12e132f6d16a7
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-11-12 17:25:25 -0800

    Update sb7.py

--- sb7.py
+++ sb7.py
@@ -1,30 +1,27 @@
+import time
 import datetime
 from ccxt import kucoinfutures as kcf
 from pandas import DataFrame as dataframe
-from ta import volatility, momentum, trend
+from ta import momentum, trend, volatility, volume
 
-LEVERAGE = 10
-LOTS = 10
-TIMEFRAMES = ['1m']
-COINS = ['LRC', 'BAND', 'YGG', 'OP', 'OCEAN', 'MATIC']
+lever = 20
+tf = '1m'
+coin = 'DOGE/USDT:USDT'
+lots = 10
+
+
 exchange = kcf({
     'apiKey': '',
     'secret': '',
     'password': '',
-    'adjustForTimeDifference': True})
+    'adjustForTimeDifference': True,
+})
 
-coins = []
-if 'all' in COINS:
-    for v in exchange.load_markets():
-        if '/USDT:USDT' in v:
-            coins.append(v)
-else:
-    for item in COINS:
-        coins.append(str(f"{item}/USDT:USDT"))
-COINS = coins
+exchange.load_markets()
 
 
 def getData(coin, tf):
+    time.sleep(exchange.rateLimit / 1000)
     data = exchange.fetch_ohlcv(coin, tf, limit=500)
     df = {}
     for i, col in enumerate(['date', 'open', 'high', 'low', 'close',
@@ -42,90 +39,72 @@ def getData(coin, tf):
 def getPositions():
     positions = exchange.fetch_positions()
     df = {}
-    for coin in COINS:
-        df[coin] = {}
-        for _, col in enumerate(['contracts', 'side', 'percentage']):
-            df[coin][col] = 0
-            for (_, v) in enumerate(positions):
-                if v['symbol'] == coin:
-                    df[coin][col] = v[col]
+    df[coin] = {}
+    for _, col in enumerate(['contracts', 'side', 'percentage', 'unrealizedPnl']):
+        df[coin][col] = 0
+        for (_, v) in enumerate(positions):
+            if v['symbol'] == coin:
+                df[coin][col] = v[col]
         DF = dataframe(df)
     return DF
 
 
-def buy():
-    if getPositions()[coin]['side'] != 'short':
-        try:
-            return exchange.create_limit_order(
-                coin, 'buy', LOTS,
-                exchange.fetch_order_book(coin)['bids'][0][0],
-                {'leverage': LEVERAGE})
-        except Exception as e:
-            print(e)
-    elif getPositions()[coin]['side'] == 'short':
-        try:
-            return exchange.create_limit_order(
-                coin, 'buy', getPositions()[coin]['contracts'],
-                exchange.fetch_order_book(coin)['bids'][0][0],
-                {'closeOrder': True, 'reduceOnly': True})
-        except Exception as e:
-            print(e)
+class order:
+    def buy():
+        bid = exchange.fetch_order_book(coin)['bids'][0][0]
+        if getPositions()[coin]['side'] != 'short':
+            exchange.create_limit_order(
+                coin, 'buy', lots,
+                bid, {'leverage': lever})
+        elif getPositions()[coin]['side'] == 'short':
+            exchange.create_limit_order(coin, 'buy', getPositions()[coin]['contracts'], bid, {
+                                        'closeOrder': True, 'reduceOnly': True})
+
+    def sell():
+        ask = exchange.fetch_order_book(coin)['asks'][0][0]
+        if getPositions()[coin]['side'] != 'long':
+            exchange.create_limit_order(
+                coin, 'sell', lots, ask, {'leverage': lever})
+        elif getPositions()[coin]['side'] == 'long':
+            exchange.create_limit_order(coin, 'sell', getPositions()[coin]['contracts'], ask, {
+                                        'closeOrder': True, 'reduceOnly': True})
 
 
-def sell():
-    if getPositions()[coin]['side'] != 'long':
-        try:
-            return exchange.create_limit_order(
-                coin, 'sell', LOTS,
-                exchange.fetch_order_book(coin)['asks'][0][0],
-                {'leverage': LEVERAGE})
-        except Exception as e:
-            print(e)
-    elif getPositions()[coin]['side'] == 'long':
-        try:
-            return exchange.create_limit_order(
-                coin, 'sell', getPositions()[coin]['contracts'],
-                exchange.fetch_order_book(coin)['asks'][0][0],
-                {'closeOrder': True, 'reduceOnly': True})
-        except Exception as e:
-            print(e)
+def open(period=-1):
+    return (getData(coin, tf)['close'].iloc[period-1] +
+            getData(coin, tf)['open'].iloc[period-1])/2
 
 
-def ema(ohlc='close', window=8, period=1):
-    return trend.ema_indicator(getData(coin, tf)[ohlc], window).iloc[-period]
+def close(period=-1):
+    return (getData(coin, tf)['close'].iloc[period] +
+            getData(coin, tf)['high'].iloc[period] +
+            getData(coin, tf)['low'].iloc[period] +
+            getData(coin, tf)['open'].iloc[period])/4
 
 
-def bands(window=20, dev=1, period=1):
-    return {
-        'pb': volatility.bollinger_pband(
-            getData(coin, tf)['close'], window, dev).iloc[-period],
-        'dn': volatility.bollinger_lband(
-            getData(coin, tf)['close'], window, dev).iloc[-period],
-        'up': volatility.bollinger_hband(
-            getData(coin, tf)['close'], window, dev).iloc[-period]
-    }
+def mfi(window=14, period=-1):
+    return volume.money_flow_index(getData(coin, tf)['high'], getData(coin, tf)['low'], getData(coin, tf)['close'], getData(coin, tf)['volume'], window).iloc[period]
 
 
-def rsi(window=2, period=1):
-    return momentum.rsi(getData(coin, tf)['close'], window).iloc[-period]
+def vwap(period=-1):
+    return volume.volume_weighted_average_price(
+        getData(coin, tf)['high'], getData(coin, tf)['low'], getData(coin, tf)['close'], getData(coin, tf)['volume']).iloc[period]
 
 
 while True:
-    for coin in COINS:
-        exchange.cancel_all_orders()
-        print(dataframe(getPositions()))
-        for tf in TIMEFRAMES:
-            print(dataframe(getPositions()[coin]))
-            # LONG
-            if bands()['pb'] > 1 and rsi() > 70:
-                while ema('close', 1) > ema() > ema('close', 200):
-                    buy()
-            while (getPositions()[coin]['side'] == 'long'):
-                sell()
-
-            # SHORT
-            while bands()['pb'] < 0 and rsi() < 30:
-                while ema('close', 1) < ema() < ema('close', 200):
-                    sell()
-            while getPositions()[coin]['side'] == 'short':
-                buy()
+    try:
+        print(getPositions())
+        while close() > vwap() and mfi() > 50 and open() < close():
+            print(getPositions())
+            order.buy()
+            if open() > close() or close() < vwap():
+                order.sell()
+                break
+        while close() < vwap() and mfi() < 50 and open() > close():
+            print(getPositions())
+            order.sell()
+            if open() < close() or close() > vwap():
+                order.buy()
+                break
+    except Exception as e:
+        print(e)
